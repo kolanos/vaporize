283005f89cc5e74c80e81bcd97985f95c6b0bfdb
Michael Lavers <kolanos@gmail.com>
2012-07-13 18:35:44 -0700

    Adding basic tests for vaporize.servers

--- tests/test_flavors.py
+++ tests/test_flavors.py
@@ -7,62 +7,61 @@ import vaporize
 
 
 def test_list():
-    expected  = '{"flavors":[{"id":1,"name":"256 server"},{"id":2,"name":"512 server"},{"id":3,"name":"1GB server"},{"id":4,"name":"2GB server"},{"id":5,"name":"4GB server"},{"id":6,"name":"8GB server"},{"id":7,"name":"15.5GB server"},{"id":8,"name":"30GB server"}]}'
+    expected = '{"servers":[{"id":12345,"name":"foo"},{"id":12346,"name":"bar"},{"id":12347,"name":"baz"}]}'
     handle_mock = partial(handle_request_mock, expected)
-    with mock(vaporize.flavors, handle_request=handle_mock):
-        flavors = vaporize.flavors.list()
-    assert isinstance(flavors, list)
-    assert len(flavors) > 0
-    for flavor in flavors:
-        assert isinstance(flavor, vaporize.flavors.Flavor)
-        assert hasattr(flavor, 'id')
-        assert hasattr(flavor, 'name')
+    with mock(vaporize.servers, handle_request=handle_mock):
+        servers = vaporize.servers.list()
+    assert isinstance(servers, list)
+    assert len(servers) > 0
+    for server in servers:
+        assert isinstance(server, vaporize.servers.Server)
+        assert hasattr(server, 'id')
+        assert hasattr(server, 'name')
 
 
 def test_list_detail():
-    expected  = '{"flavors":[{"id":1,"ram":256,"disk":10,"name":"256 server"},{"id":2,"ram":512,"disk":20,"name":"512 server"},{"id":3,"ram":1024,"disk":40,"name":"1GB server"},{"id":4,"ram":2048,"disk":80,"name":"2GB server"},{"id":5,"ram":4096,"disk":160,"name":"4GB server"},{"id":6,"ram":8192,"disk":320,"name":"8GB server"},{"id":7,"ram":15872,"disk":620,"name":"15.5GB server"},{"id":8,"ram":30720,"disk":1200,"name":"30GB server"}]}'
+    expected = '{"servers":[{"progress":0,"id":12345,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"foo","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.1"],"private":["10.0.0.1"]},"metadata":{}},{"progress":0,"id":12346,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"bar","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.2"],"private":["10.0.0.2"]},"metadata":{}},{"progress":0,"id":12347,"imageId":112,"flavorId":5,"status":"ACTIVE","name":"baz","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.3"],"private":["10.0.0.3"]},"metadata":{}}]}'
     handle_mock = partial(handle_request_mock, expected)
-    with mock(vaporize.flavors, handle_request=handle_mock):
-        flavors = vaporize.flavors.list(detail=True)
-    assert isinstance(flavors, list)
-    assert len(flavors) > 0
-    for flavor in flavors:
-        assert isinstance(flavor, vaporize.flavors.Flavor)
-        assert hasattr(flavor, 'id')
-        assert hasattr(flavor, 'name')
-        assert hasattr(flavor, 'disk')
-        assert hasattr(flavor, 'ram')
+    with mock(vaporize.servers, handle_request=handle_mock):
+        servers = vaporize.servers.list(detail=True)
+    assert isinstance(servers, list)
+    assert len(servers) > 0
+    for server in servers:
+        assert isinstance(server, vaporize.servers.Server)
+        assert hasattr(server, 'id')
+        assert hasattr(server, 'name')
+        assert hasattr(server, 'image_id')
+        assert hasattr(server, 'flavor_id')
+        assert hasattr(server, 'addresses')
+        assert hasattr(server, 'host_id')
+        assert hasattr(server, 'status')
 
 
 def test_list_limit():
-    expected  = '{"flavors":[{"id":1,"name":"256 server"},{"id":2,"name":"512 server"},{"id":3,"name":"1GB server"},{"id":4,"name":"2GB server"},{"id":5,"name":"4GB server"}]}'
+    expected = '{"servers":[{"id":12345,"name":"foo"},{"id":12346,"name":"bar"}]}'
     handle_mock = partial(handle_request_mock, expected)
-    with mock(vaporize.flavors, handle_request=handle_mock):
-        flavors = vaporize.flavors.list(limit=5)
-    assert isinstance(flavors, list)
-    assert len(flavors) == 5
+    with mock(vaporize.servers, handle_request=handle_mock):
+        servers = vaporize.servers.list(limit=2)
+    assert isinstance(servers, list)
+    assert len(servers) == 2
 
 
 def test_list_offset():
-    expected  = '{"flavors":[{"id":6,"name":"8GB server"},{"id":7,"name":"15.5GB server"},{"id":8,"name":"30GB server"}]}'
+    expected = '{"servers":[{"id":12346,"name":"bar"},{"id":12347,"name":"baz"}]}'
     handle_mock = partial(handle_request_mock, expected)
-    with mock(vaporize.flavors, handle_request=handle_mock):
-        flavors = vaporize.flavors.list(offset=5)
-    assert isinstance(flavors, list)
-    assert flavors[0].id == 6
+    with mock(vaporize.servers, handle_request=handle_mock):
+        servers = vaporize.servers.list(offset=1)
+    assert isinstance(servers, list)
+    assert servers[0].id == 12346
 
 
 def test_get():
-    expected  = '{"flavor":{"id":1,"ram":256,"disk":10,"name":"256 server"}}'
+    expected = '{"server":{"progress":0,"id":12345,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"foo","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.1"],"private":["10.0.0.1"]},"metadata":{}}}'
     handle_mock = partial(handle_request_mock, expected)
-    with mock(vaporize.flavors, handle_request=handle_mock):
-        flavor = vaporize.flavors.get(1)
-    assert isinstance(flavor, vaporize.flavors.Flavor)
-    assert hasattr(flavor, 'id')
-    assert flavor.id == 1
-    assert hasattr(flavor, 'name')
-    assert flavor.name == '256 server'
-    assert hasattr(flavor, 'disk')
-    assert flavor.disk == 10
-    assert hasattr(flavor, 'ram')
-    assert flavor.ram == 256
+    with mock(vaporize.servers, handle_request=handle_mock):
+        server = vaporize.servers.get(12345)
+    assert isinstance(server, vaporize.servers.Server)
+    assert hasattr(server, 'id')
+    assert server.id == 12345
+    assert hasattr(server, 'name')
+    assert server.name == 'foo'
