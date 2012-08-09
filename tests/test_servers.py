f1c349287aedaf16769892ac31d530322f8ec480
Michael Lavers <kolanos@gmail.com>
2012-08-08 23:27:56 -0700

    simplifying tests

--- tests/test_servers.py
+++ tests/test_servers.py
@@ -1,15 +1,34 @@
 from functools import partial
-
-from .mock import mock, handle_request_mock
+import json
 
 import vaporize
+
+
+def get_url_mock(service):
+    return 'http://localhost/'
+vaporize.servers.get_url = get_url_mock
+
+
+def handle_request_mock(status_code, content, verb, url, data=None, wrapper=None, container=None, **kwargs):
+    response = vaporize.utils.DotDict(status_code=status_code, content=content)
+    if response.status_code not in [200, 201, 202, 203, 204]:
+        vaporize.exceptions.handle_exception(response.status_code, response.content)
+    if not response.content:
+        return True
+    content = json.loads(response.content)
+    if container and isinstance(content[container], list):
+        return [wrapper(i, **kwargs) for i in content[container]]
+    elif container is None:
+        return wrapper(content, **kwargs)
+    else:
+        return wrapper(content[container], **kwargs)
 
 
 def test_flavor_list():
     expected  = '{"flavors":[{"id":1,"name":"256 server"},{"id":2,"name":"512 server"},{"id":3,"name":"1GB server"},{"id":4,"name":"2GB server"},{"id":5,"name":"4GB server"},{"id":6,"name":"8GB server"},{"id":7,"name":"15.5GB server"},{"id":8,"name":"30GB server"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        flavors = vaporize.servers.Flavor.list()
+    vaporize.servers.handle_request = handle_mock
+    flavors = vaporize.servers.Flavor.list()
     assert isinstance(flavors, list)
     assert len(flavors) > 0
     for flavor in flavors:
@@ -21,8 +40,8 @@ def test_flavor_list():
 def test_flavor_list_detail():
     expected  = '{"flavors":[{"id":1,"ram":256,"disk":10,"name":"256 server"},{"id":2,"ram":512,"disk":20,"name":"512 server"},{"id":3,"ram":1024,"disk":40,"name":"1GB server"},{"id":4,"ram":2048,"disk":80,"name":"2GB server"},{"id":5,"ram":4096,"disk":160,"name":"4GB server"},{"id":6,"ram":8192,"disk":320,"name":"8GB server"},{"id":7,"ram":15872,"disk":620,"name":"15.5GB server"},{"id":8,"ram":30720,"disk":1200,"name":"30GB server"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        flavors = vaporize.servers.Flavor.list(detail=True)
+    vaporize.servers.handle_request = handle_mock
+    flavors = vaporize.servers.Flavor.list(detail=True)
     assert isinstance(flavors, list)
     assert len(flavors) > 0
     for flavor in flavors:
@@ -36,8 +55,8 @@ def test_flavor_list_detail():
 def test_flavor_list_limit():
     expected  = '{"flavors":[{"id":1,"name":"256 server"},{"id":2,"name":"512 server"},{"id":3,"name":"1GB server"},{"id":4,"name":"2GB server"},{"id":5,"name":"4GB server"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        flavors = vaporize.servers.Flavor.list(limit=5)
+    vaporize.servers.handle_request = handle_mock
+    flavors = vaporize.servers.Flavor.list(limit=5)
     assert isinstance(flavors, list)
     assert len(flavors) == 5
 
@@ -45,8 +64,8 @@ def test_flavor_list_limit():
 def test_flavor_list_offset():
     expected  = '{"flavors":[{"id":6,"name":"8GB server"},{"id":7,"name":"15.5GB server"},{"id":8,"name":"30GB server"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        flavors = vaporize.servers.Flavor.list(offset=5)
+    vaporize.servers.handle_request = handle_mock
+    flavors = vaporize.servers.Flavor.list(offset=5)
     assert isinstance(flavors, list)
     assert flavors[0].id == 6
 
@@ -54,8 +73,8 @@ def test_flavor_list_offset():
 def test_flavor_get():
     expected  = '{"flavor":{"id":1,"ram":256,"disk":10,"name":"256 server"}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        flavor = vaporize.servers.Flavor.get(1)
+    vaporize.servers.handle_request = handle_mock
+    flavor = vaporize.servers.Flavor.get(1)
     assert isinstance(flavor, vaporize.servers.Flavor)
     assert hasattr(flavor, 'id')
     assert flavor.id == 1
@@ -70,8 +89,8 @@ def test_flavor_get():
 def test_image_list():
     expected  = '{"images":[{"id":31,"name":"Windows Server 2008 SP2 (32-bit)"},{"id":85,"name":"Windows Server 2008 R2 (64-bit)"},{"id":100,"name":"Arch 2011.10"},{"id":89,"name":"Windows Server 2008 R2 + SQL Server 2008 R2 Web"},{"id":126,"name":"Fedora 17"},{"id":108,"name":"Gentoo 11.0"},{"id":109,"name":"openSUSE 12"},{"id":110,"name":"Red Hat Enterprise Linux 5.5"},{"id":56,"name":"Windows Server 2008 SP2 (32-bit) + SQL Server 2008 R2 Standard"},{"id":120,"name":"Fedora 16"},{"id":119,"name":"Ubuntu 11.10"},{"id":122,"name":"CentOS 6.2"},{"id":104,"name":"Debian 6 (Squeeze)"},{"id":91,"name":"Windows Server 2008 R2 + SQL Server 2012 Standard"},{"id":115,"name":"Ubuntu 11.04"},{"id":118,"name":"CentOS 6.0"},{"id":86,"name":"Windows Server 2008 R2 + SQL Server 2008 R2 Standard"},{"id":92,"name":"Windows Server 2008 R2 + SQL Server 2012 Web"},{"id":112,"name":"Ubuntu 10.04 LTS"},{"id":24,"name":"Windows Server 2008 SP2 (64-bit)"},{"id":121,"name":"CentOS 5.8"},{"id":111,"name":"Red Hat Enterprise Linux 6"},{"id":116,"name":"Fedora 15"},{"id":125,"name":"Ubuntu 12.04 LTS"},{"id":114,"name":"CentOS 5.6"},{"id":103,"name":"Debian 5 (Lenny)"},{"id":57,"name":"Windows Server 2008 SP2 (64-bit) + SQL Server 2008 R2 Standard"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        images = vaporize.servers.Image.list()
+    vaporize.servers.handle_request = handle_mock
+    images = vaporize.servers.Image.list()
     assert isinstance(images, list)
     assert len(images) > 0
     for image in images:
@@ -83,8 +102,8 @@ def test_image_list():
 def test_image_list_detail():
     expected  = '{"images":[{"id":31,"status":"ACTIVE","updated":"2010-01-26T12:07:44-06:00","name":"Windows Server 2008 SP2 (32-bit)"},{"id":85,"status":"ACTIVE","updated":"2010-01-26T12:07:17-06:00","name":"Windows Server 2008 R2 (64-bit)"},{"id":100,"status":"ACTIVE","updated":"2011-09-12T09:09:23-05:00","name":"Arch 2011.10"},{"id":89,"status":"ACTIVE","updated":"2011-10-04T08:39:34-05:00","name":"Windows Server 2008 R2 + SQL Server 2008 R2 Web"},{"id":126,"status":"ACTIVE","updated":"2012-05-29T17:11:45-05:00","name":"Fedora 17"},{"id":108,"status":"ACTIVE","updated":"2011-11-01T08:32:30-05:00","name":"Gentoo 11.0"},{"id":109,"status":"ACTIVE","updated":"2011-11-03T06:28:56-05:00","name":"openSUSE 12"},{"id":110,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"Red Hat Enterprise Linux 5.5"},{"id":56,"status":"ACTIVE","updated":"2010-09-17T07:12:56-05:00","name":"Windows Server 2008 SP2 (32-bit) + SQL Server 2008 R2 Standard"},{"id":120,"status":"ACTIVE","updated":"2012-01-03T04:39:05-06:00","name":"Fedora 16"},{"id":119,"status":"ACTIVE","updated":"2011-11-03T08:55:15-05:00","name":"Ubuntu 11.10"},{"id":122,"status":"ACTIVE","updated":"2012-02-06T04:34:21-06:00","name":"CentOS 6.2"},{"id":104,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"Debian 6 (Squeeze)"},{"id":91,"status":"ACTIVE","updated":"2012-04-24T16:44:01-05:00","name":"Windows Server 2008 R2 + SQL Server 2012 Standard"},{"id":115,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"Ubuntu 11.04"},{"id":118,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"CentOS 6.0"},{"id":86,"status":"ACTIVE","updated":"2010-09-17T07:19:20-05:00","name":"Windows Server 2008 R2 + SQL Server 2008 R2 Standard"},{"id":92,"status":"ACTIVE","updated":"2012-04-24T16:44:01-05:00","name":"Windows Server 2008 R2 + SQL Server 2012 Web"},{"id":112,"status":"ACTIVE","updated":"2011-04-21T10:24:01-05:00","name":"Ubuntu 10.04 LTS"},{"id":24,"status":"ACTIVE","updated":"2010-01-26T12:07:04-06:00","name":"Windows Server 2008 SP2 (64-bit)"},{"id":121,"status":"ACTIVE","updated":"2012-05-04T10:51:28-05:00","name":"CentOS 5.8"},{"id":111,"status":"ACTIVE","updated":"2011-09-12T10:53:12-05:00","name":"Red Hat Enterprise Linux 6"},{"id":116,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"Fedora 15"},{"id":125,"status":"ACTIVE","updated":"2012-05-03T07:21:06-05:00","name":"Ubuntu 12.04 LTS"},{"id":114,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"CentOS 5.6"},{"id":103,"status":"ACTIVE","updated":"2011-08-17T05:11:30-05:00","name":"Debian 5 (Lenny)"},{"id":57,"status":"ACTIVE","updated":"2010-09-17T07:16:25-05:00","name":"Windows Server 2008 SP2 (64-bit) + SQL Server 2008 R2 Standard"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        images = vaporize.servers.Image.list(detail=True)
+    vaporize.servers.handle_request = handle_mock
+    images = vaporize.servers.Image.list(detail=True)
     assert isinstance(images, list)
     assert len(images) > 0
     for image in images:
@@ -98,8 +117,8 @@ def test_image_list_detail():
 def test_image_list_limit():
     expected  = '{"images":[{"id":31,"name":"Windows Server 2008 SP2 (32-bit)"},{"id":85,"name":"Windows Server 2008 R2 (64-bit)"},{"id":100,"name":"Arch 2011.10"},{"id":89,"name":"Windows Server 2008 R2 + SQL Server 2008 R2 Web"},{"id":126,"name":"Fedora 17"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        images = vaporize.servers.Image.list(limit=5)
+    vaporize.servers.handle_request = handle_mock
+    images = vaporize.servers.Image.list(limit=5)
     assert isinstance(images, list)
     assert len(images) == 5
 
@@ -107,8 +126,8 @@ def test_image_list_limit():
 def test_image_list_offset():
     expected  = '{"images":[{"id":108,"name":"Gentoo 11.0"},{"id":109,"name":"openSUSE 12"},{"id":110,"name":"Red Hat Enterprise Linux 5.5"},{"id":56,"name":"Windows Server 2008 SP2 (32-bit) + SQL Server 2008 R2 Standard"},{"id":120,"name":"Fedora 16"},{"id":119,"name":"Ubuntu 11.10"},{"id":122,"name":"CentOS 6.2"},{"id":104,"name":"Debian 6 (Squeeze)"},{"id":91,"name":"Windows Server 2008 R2 + SQL Server 2012 Standard"},{"id":115,"name":"Ubuntu 11.04"},{"id":118,"name":"CentOS 6.0"},{"id":86,"name":"Windows Server 2008 R2 + SQL Server 2008 R2 Standard"},{"id":92,"name":"Windows Server 2008 R2 + SQL Server 2012 Web"},{"id":112,"name":"Ubuntu 10.04 LTS"},{"id":24,"name":"Windows Server 2008 SP2 (64-bit)"},{"id":121,"name":"CentOS 5.8"},{"id":111,"name":"Red Hat Enterprise Linux 6"},{"id":116,"name":"Fedora 15"},{"id":125,"name":"Ubuntu 12.04 LTS"},{"id":114,"name":"CentOS 5.6"},{"id":103,"name":"Debian 5 (Lenny)"},{"id":57,"name":"Windows Server 2008 SP2 (64-bit) + SQL Server 2008 R2 Standard"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        images = vaporize.servers.Image.list(offset=5)
+    vaporize.servers.handle_request = handle_mock
+    images = vaporize.servers.Image.list(offset=5)
     assert isinstance(images, list)
     assert images[0].id == 108
 
@@ -116,8 +135,8 @@ def test_image_list_offset():
 def test_image_get():
     expected  = '{"image":{"id":112,"status":"ACTIVE","created":"2011-04-21T10:24:01-05:00","updated":"2011-04-21T10:24:01-05:00","name":"Ubuntu 10.04 LTS"}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        image = vaporize.servers.Image.get(112)
+    vaporize.servers.handle_request = handle_mock
+    image = vaporize.servers.Image.get(112)
     assert isinstance(image, vaporize.servers.Image)
     assert hasattr(image, 'id')
     assert image.id == 112
@@ -130,8 +149,8 @@ def test_image_get():
 def test_image_create():
     expected = '{"image":{"id":12345,"status":"QUEUED","updated":"2012-07-13T17:26:24-05:00","name":"testing","serverId":54321}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        image = vaporize.servers.Image.create('testing', 54321)
+    vaporize.servers.handle_request = handle_mock
+    image = vaporize.servers.Image.create('testing', 54321)
     assert isinstance(image, vaporize.servers.Image)
     assert hasattr(image, 'id')
     assert image.id == 12345
@@ -146,9 +165,9 @@ def test_image_create():
 def test_image_create_server_instance():
     expected = '{"image":{"id":12345,"status":"QUEUED","updated":"2012-07-13T17:26:24-05:00","name":"testing","serverId":54321}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        server = vaporize.servers.Server(id=54321)
-        image = vaporize.servers.Image.create('testing', server)
+    vaporize.servers.handle_request = handle_mock
+    server = vaporize.servers.Server(id=54321)
+    image = vaporize.servers.Image.create('testing', server)
     assert isinstance(image, vaporize.servers.Image)
     assert hasattr(image, 'id')
     assert image.id == 12345
@@ -163,9 +182,9 @@ def test_image_create_server_instance():
 def test_image_reload():
     expected = '{"image":{"progress":100,"id":12345,"status":"ACTIVE","created":"2012-07-13T17:26:23-05:00","updated":"2012-07-13T17:29:10-05:00","name":"testing"}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        image = vaporize.servers.Image.get(12345)
-        image.reload()
+    vaporize.servers.handle_request = handle_mock
+    image = vaporize.servers.Image.get(12345)
+    image.reload()
     assert isinstance(image, vaporize.servers.Image)
     assert hasattr(image, 'id')
     assert image.id == 12345
@@ -182,8 +201,8 @@ def test_image_reload_without_id():
 def test_server_list():
     expected = '{"servers":[{"id":12345,"name":"foo"},{"id":12346,"name":"bar"},{"id":12347,"name":"baz"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        servers = vaporize.servers.Server.list()
+    vaporize.servers.handle_request = handle_mock
+    servers = vaporize.servers.Server.list()
     assert isinstance(servers, list)
     assert len(servers) > 0
     for server in servers:
@@ -195,8 +214,8 @@ def test_server_list():
 def test_server_list_detail():
     expected = '{"servers":[{"progress":0,"id":12345,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"foo","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.1"],"private":["10.0.0.1"]},"metadata":{}},{"progress":0,"id":12346,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"bar","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.2"],"private":["10.0.0.2"]},"metadata":{}},{"progress":0,"id":12347,"imageId":112,"flavorId":5,"status":"ACTIVE","name":"baz","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.3"],"private":["10.0.0.3"]},"metadata":{}}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        servers = vaporize.servers.Server.list(detail=True)
+    vaporize.servers.handle_request = handle_mock
+    servers = vaporize.servers.Server.list(detail=True)
     assert isinstance(servers, list)
     assert len(servers) > 0
     for server in servers:
@@ -213,8 +232,8 @@ def test_server_list_detail():
 def test_server_list_limit():
     expected = '{"servers":[{"id":12345,"name":"foo"},{"id":12346,"name":"bar"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        servers = vaporize.servers.Server.list(limit=2)
+    vaporize.servers.handle_request = handle_mock
+    servers = vaporize.servers.Server.list(limit=2)
     assert isinstance(servers, list)
     assert len(servers) == 2
 
@@ -222,8 +241,8 @@ def test_server_list_limit():
 def test_server_list_offset():
     expected = '{"servers":[{"id":12346,"name":"bar"},{"id":12347,"name":"baz"}]}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        servers = vaporize.servers.Server.list(offset=1)
+    vaporize.servers.handle_request = handle_mock
+    servers = vaporize.servers.Server.list(offset=1)
     assert isinstance(servers, list)
     assert servers[0].id == 12346
 
@@ -231,8 +250,8 @@ def test_server_list_offset():
 def test_server_get():
     expected = '{"server":{"progress":0,"id":12345,"imageId":112,"flavorId":4,"status":"ACTIVE","name":"foo","hostId":"asdfasdfasdfasdfasdf","addresses":{"public":["198.0.0.1"],"private":["10.0.0.1"]},"metadata":{}}}'
     handle_mock = partial(handle_request_mock, 200, expected)
-    with mock(vaporize.servers, handle_request=handle_mock):
-        server = vaporize.servers.Server.get(12345)
+    vaporize.servers.handle_request = handle_mock
+    server = vaporize.servers.Server.get(12345)
     assert isinstance(server, vaporize.servers.Server)
     assert hasattr(server, 'id')
     assert server.id == 12345
